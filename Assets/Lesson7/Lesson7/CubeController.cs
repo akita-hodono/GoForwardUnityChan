e93118a535ebdc71eb911428ba11e3d40d6f6649
akita-hodono <akita-hodono@iteen.jp>
2022-10-28 14:35:47 +0900

    最初のコミット

---
+++ Assets/Lesson7/Lesson7/CubeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    public AudioClip block;
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AudioSourceのコンポーネントを取得
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground"||collision.gameObject.tag=="Cube")
+        {
+            
+            //課題：キューブの効果音を鳴らす
+            audioSource.PlayOneShot(block);
+        }
+
+      
+    }
+}
